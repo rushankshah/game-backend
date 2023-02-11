e49d618076c2b210524da1464b5d07be4302b5df
rushankshah <rushankshah65@gmail.com>
2023-02-10 23:27:25 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./cs-526-team-undecided-firebase-adminsdk-9dgr5-3b2075b0ef.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+function getDateAndTime() {
+  var today = new Date();
+  var date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + " " + time;
+  return dateTime;
+}
+
+app.post("/", (req, res) => {
+  const { score, time } = req.body;
+  db.collection("players-data")
+    .doc(getDateAndTime())
+    .set({ score, time })
+    .then(() => {
+      console.log("Document successfully written!");
+    });
+  res.send("Successfully written to database!");
+});
+
+app.get("/", (req, res) => {
+  db.collection("players-data")
+    .get()
+    .then((snapshot) => {
+      var arr = [];
+      snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        arr.push(doc.data());
+      });
+      res.send(arr);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// db.collection("users")
+//   .doc("test")
+//   .set({
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815,
+//   })
+//   .then(() => {
+//     console.log("Document successfully written!");
+//   });
